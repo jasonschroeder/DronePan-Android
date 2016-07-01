54b37663bacb779a1ef9b0ea2fa072fdd1bd7b50
Joe Espindola <joe@061f.co>
2016-06-30 21:54:39 -0300

    camera code almost finished

--- Drone Pan Android/src/main/java/com/dronepan/AndroidApp/DJIController.java
+++ Drone Pan Android/src/main/java/com/dronepan/AndroidApp/DJIController.java
@@ -1,12 +1,19 @@
 package com.dronepan.AndroidApp;
 
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.graphics.SurfaceTexture;
 import android.os.Handler;
 import android.os.Looper;
 import android.util.Log;
+import android.view.TextureView;
 import android.widget.Toast;
 
+import dji.sdk.Camera.DJICamera;
+import dji.sdk.Codec.DJICodecManager;
+import dji.sdk.Products.DJIAircraft;
 import dji.sdk.SDKManager.DJISDKManager;
 import dji.sdk.base.DJIBaseComponent;
 import dji.sdk.base.DJIBaseProduct;
@@ -20,16 +27,139 @@ public class DJIController {
     private static final String TAG = DJIController.class.getName();
     public static final String FLAG_CONNECTION_CHANGE = "dji_sdk_connection_change";
 
+    // PRODUCT
     private static DJIBaseProduct mProduct;
     private Handler mHandler;
-    private Context mainCtx;
+    // MAIN ACTIVITY
+    private DronePanMainActivity mainCtx;
 
-    public DJIController(Context ctx) {
+    protected DJICamera.CameraReceivedVideoDataCallback mReceivedVideoDataCallBack = null;
+    protected DJICodecManager mCodecManager = null;
+
+    public DJIController(DronePanMainActivity ctx) {
         // DJI SDK MANAGER
         mHandler = new Handler(Looper.getMainLooper());
 
         mainCtx = ctx;
         DJISDKManager.getInstance().initSDKManager(mainCtx, mDJISDKMangerCallback);
+
+        // RECEIVED VIDEO DATA CALLBACK
+        mReceivedVideoDataCallBack = new DJICamera.CameraReceivedVideoDataCallback() {
+            @Override public void onResult(byte[] videoBuffer, int size) {
+                if(mCodecManager != null) {
+                    mCodecManager.sendDataToDecoder(videoBuffer, size);
+                }
+                else {
+                    Log.e(TAG, "Codec Manager is null");
+                }
+            }
+        };
+
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(DJIController.FLAG_CONNECTION_CHANGE);
+        mainCtx.registerReceiver(mReceiver, filter);
+    }
+
+    protected BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+
+            // UPDATE MAIN ACTIVITY TITLE BAR
+            mainCtx.updateTitleBar();
+
+            //updateTitleBar();
+            //onProductChange();
+        }
+    };
+
+    // GET PRODUCT INSTANCE
+    public static synchronized DJIBaseProduct getProductInstance() {
+        if(null == mProduct) {
+            mProduct = DJISDKManager.getInstance().getDJIProduct();
+        }
+
+        return mProduct;
+    }
+
+    // GET CAMERA INSTANCE
+    public static  synchronized DJICamera getCameraInstance() {
+        if(getProductInstance() == null) {
+            return null;
+        }
+
+        return getProductInstance().getCamera();
+    }
+
+    // IS AIRCRAFT CONNECTED
+    public boolean isAircrafConnected() {
+        DJIBaseProduct product = getProductInstance();
+
+        if(product != null) {
+            // IF PRODUCT IS CONNECTED
+            if(product.isConnected()) {
+               return true;
+            }
+        }
+
+        return false;
+    }
+
+    // GET AIRCRAFT MODEL
+    public String getAircraftModel() {
+        DJIBaseProduct product = getProductInstance();
+
+        if(product != null) {
+            // IF PRODUCT IS CONNECTED
+            if(product.isConnected()) {
+                return getProductInstance().getModel()+"";
+            }
+        }
+
+        // RETURN EMPTY STRING
+        return "";
+    }
+
+    // SET VIDEO SURFACE
+    public void setVideoSurface(TextureView textureView) {
+        DJIBaseProduct product = getProductInstance();
+
+        if(product == null || !product.isConnected()) {
+            Log.e(TAG, "No aircraft connected");
+        }
+        else {
+            if(null != textureView) {
+                DJICamera camera = product.getCamera();
+                if (camera != null){
+                    // Set the callback
+                    camera.setDJICameraReceivedVideoDataCallback(mReceivedVideoDataCallBack);
+                }
+            }
+        }
+    }
+
+    // UNSET VIDEO SURFACE
+    public void unsetVideoSurface() {
+        DJICamera camera = getCameraInstance();
+
+        if(camera != null) {
+            // UNSET CAMERA VIDEO DATA CALLBACK
+            camera.setDJICameraReceivedVideoDataCallback(null);
+        }
+    }
+
+    // SET CODEC SURFACE TEXTURE
+    public void setCodecSurface(SurfaceTexture surface, int width, int height) {
+        if(mCodecManager == null) {
+            mCodecManager = new DJICodecManager(mainCtx, surface, width, height);
+        }
+    }
+
+    //  UNSET CODEC SURFACE TEXTURE
+    public void unsetCodecSurface() {
+        if(mCodecManager != null) {
+            mCodecManager.cleanSurface();
+            mCodecManager = null;
+        }
     }
 
     private DJISDKManager.DJISDKManagerCallback mDJISDKMangerCallback = new DJISDKManager.DJISDKManagerCallback() {
@@ -45,8 +175,8 @@ public class DJIController {
                 handler.post(new Runnable() {
                     @Override
                     public void run() {
-                        Log.d("DronePan", "SUCCESS");
-                        Toast.makeText(mainCtx.getApplicationContext(), "SUCCESS REGISTERING SDK", Toast.LENGTH_LONG).show();
+                        Log.d("DronePan", "SUCCESS REGISTRATION SDK");
+                        mainCtx.showToast("SUCCESS REGISTRATION SDK");
                     }
                 });
             }
@@ -55,13 +185,14 @@ public class DJIController {
                 handler.post(new Runnable() {
                     @Override
                     public void run() {
-                        Log.d("DronePan", "ERROR");
-                        Toast.makeText(mainCtx.getApplicationContext(), "ERROR REGISTERING SDK", Toast.LENGTH_LONG).show();
+                        Log.d("DronePan", "ERROR REGISTRATION SDK");
+                        mainCtx.showToast("ERROR REGISTERING SDK");
                     }
                 });
             }
         }
 
+        // ON PRODUCT CHANGED
         @Override public void onProductChanged(DJIBaseProduct oldProduct, DJIBaseProduct newProduct) {
             mProduct = newProduct;
 
@@ -83,6 +214,10 @@ public class DJIController {
             notifyStatusChange();
         }
     };
+
+    /*protected void onProductChange() {
+        //initPreviewer();
+    }*/
 
     private DJIBaseComponent.DJIComponentListener mDJIComponentListener = new DJIBaseComponent.DJIComponentListener() {
         @Override public void onComponentConnectivityChanged(boolean isConnected) {
--- Drone Pan Android/src/main/java/com/dronepan/AndroidApp/DronePanMainActivity.java
+++ Drone Pan Android/src/main/java/com/dronepan/AndroidApp/DronePanMainActivity.java
@@ -1,19 +1,241 @@
 package com.dronepan.AndroidApp;
 
+import android.app.Activity;
+import android.app.Application;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.graphics.SurfaceTexture;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.TextureView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+import android.widget.CompoundButton;
+import android.Manifest;
 
-public class DronePanMainActivity extends AppCompatActivity {
+import org.w3c.dom.Text;
+
+import dji.sdk.Products.DJIAircraft;
+import dji.sdk.base.DJIBaseProduct;
+
+public class DronePanMainActivity extends Activity implements TextureView.SurfaceTextureListener, View.OnClickListener {
+    private static final String TAG = DronePanMainActivity.class.getName();
     private DJIController djiController = null;
+
+    public static String DRONEPAN_ANDROID_VERSION = "0.1";
+
+    public TextView mConnectStatusTextView;
+    protected TextureView mVideoSurface = null;
+
+    public TextView recordingTime;
+    public Button mCaptureButton, mShootPhotoModeButton, mRecordVideoModeButton;
+    public ToggleButton mRecordButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // REQUEST PERMISSIONS FOR SDK SANITY
+        /*if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            AppCompatActivity.requestPermission(this,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.VIBRATE,
+                            Manifest.permission.INTERNET, Manifest.permission.ACCESS_WIFI_STATE,
+                            Manifest.permission.WAKE_LOCK, Manifest.permission.ACCESS_COARSE_LOCATION,
+                            Manifest.permission.ACCESS_NETWORK_STATE, Manifest.permission.ACCESS_FINE_LOCATION,
+                            Manifest.permission.CHANGE_WIFI_STATE, Manifest.permission.MOUNT_UNMOUNT_FILESYSTEMS,
+                            Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.SYSTEM_ALERT_WINDOW,
+                            Manifest.permission.READ_PHONE_STATE,
+                    }
+                    , 1);
+        }*/
+
+        // SET CONTENT VIEW
         setContentView(R.layout.activity_dronepan);
+
+        // START UI ELEMENTS
+        initUI();
 
         // START DJI SDK CONTROLLER
         djiController = new DJIController(this);
+
+        showToast("DJI INITIALIZED");
+
+        // RECEIVE DEVICE CONNECTION CHANGES
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(DJIController.FLAG_CONNECTION_CHANGE);
+        registerReceiver(mReceiver, filter);
     }
 
+    protected BroadcastReceiver mReceiver = new BroadcastReceiver() {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+
+            // UPDATE TITLE BAR
+            updateTitleBar();
+            // ON PRODUCT CHANGE
+            onProductChange();
+
+        }
+
+    };
+
+
+    // UPDATE TITLE BAR
+    public void updateTitleBar() {
+        if(mConnectStatusTextView == null) return;
+
+        if(djiController.isAircrafConnected()) {
+            mConnectStatusTextView.setText(djiController.getAircraftModel() + " connected!");
+        }
+        else {
+            mConnectStatusTextView.setText("No aircraft connected!");
+        }
+    }
+
+    // ON PRODUCT CHANGE SET VIDEO SURFACE
+    protected void onProductChange() {
+        setVideoSurface();
+    }
+
+    // SET VIDEO SURFACE
+    public void setVideoSurface() {
+        if (!djiController.isAircrafConnected()) {
+            Log.e(TAG, "No aircraft connected");
+        } else {
+            if (!djiController.getAircraftModel().equals(DJIBaseProduct.Model.UnknownAircraft)) {
+                djiController.setVideoSurface(mVideoSurface);
+            }
+        }
+    }
+
+    // UNSET VIDEO SURFACE
+    public void unsetVideoSurface() {
+        djiController.unsetVideoSurface();
+    }
+
+    // SHOW TOASTER
+    public void showToast(final String msg) {
+        runOnUiThread(new Runnable() {
+            public void run() {
+                Toast.makeText(DronePanMainActivity.this, msg, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    // ON RESUME
+    @Override public void onResume() {
+        super.onResume();
+
+        // SET VIDEO SURFACE
+        setVideoSurface();
+
+        // UPDATE TITLE BAR
+        updateTitleBar();
+
+        if(mVideoSurface == null) {
+            Log.e(TAG, "mVideoSurface is null");
+        }
+
+    }
+
+    // ON PAUSE
+    @Override public void onPause() {
+        // UNSET VIDEO SURFACE
+        unsetVideoSurface();
+
+        super.onPause();
+    }
+
+    // ON STOP
+    @Override public void onStop() {
+        super.onStop();
+    }
+
+    // ON RETURN
+    public void onReturn(View view) {
+        this.finish();
+    }
+
+    // ON DESTROY
+    @Override public void onDestroy() {
+        // UNSET VIDEO SURFACE
+        unsetVideoSurface();
+
+        unregisterReceiver(mReceiver);
+
+        super.onDestroy();
+    }
+
+    @Override public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
+
+    }
+
+    @Override public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) {
+
+    }
+
+    @Override public void onSurfaceTextureUpdated(SurfaceTexture surface) {
+
+    }
+
+    @Override public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
+        return true;
+    }
+
+    // START UI ELEMENTS
+    private void initUI() {
+        mConnectStatusTextView = (TextView) findViewById(R.id.ConnectStatusTextView);
+        // init mVideoSurface
+        mVideoSurface = (TextureView)findViewById(R.id.video_previewer_surface);
+
+        recordingTime = (TextView) findViewById(R.id.timer);
+        mCaptureButton = (Button) findViewById(R.id.btn_capture);
+        mRecordButton = (ToggleButton) findViewById(R.id.btn_record);
+        mShootPhotoModeButton = (Button) findViewById(R.id.btn_shoot_photo_mode);
+        mRecordVideoModeButton = (Button) findViewById(R.id.btn_record_video_mode);
+
+        if (null != mVideoSurface) {
+            mVideoSurface.setSurfaceTextureListener(this);
+        }
+
+        mCaptureButton.setOnClickListener(this);
+        mRecordButton.setOnClickListener(this);
+        mShootPhotoModeButton.setOnClickListener(this);
+        mRecordVideoModeButton.setOnClickListener(this);
+
+        recordingTime.setVisibility(View.INVISIBLE);
+
+        mRecordButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_capture:{
+                break;
+            }
+            case R.id.btn_shoot_photo_mode:{
+                break;
+            }
+            case R.id.btn_record_video_mode:{
+                break;
+            }
+            default:
+                break;
+        }
+    }
 
 }
