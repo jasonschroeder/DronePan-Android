cc4000fc1845a86344ce40e6249375da2db5db86
Jason Schroeder <jason@orangatame.com>
2016-07-25 22:23:21 -0700

    Use the NRF Logger
    See also https://github.com/NordicSemiconductor/nRF-Logger-API
    MIT licensed. Compatible with this project, which is GPLv3.

--- Drone Pan Android/src/main/java/com/dronepan/AndroidApp/DronePanApplication.java
+++ Drone Pan Android/src/main/java/com/dronepan/AndroidApp/DronePanApplication.java
@@ -1,7 +1,11 @@
 package com.dronepan.AndroidApp;
 
 import android.app.Application;
+import android.content.Context;
+import android.util.Log;
 
+import no.nordicsemi.android.log.ILogSession;
+import no.nordicsemi.android.log.Logger;
 import timber.log.Timber;
 
 /**
@@ -16,8 +20,40 @@ public class DronePanApplication extends Application {
 
         if (BuildConfig.DEBUG) {
             Timber.plant(new Timber.DebugTree());
+            Timber.plant(new NRFTee(this));
+        }
+
+    }
+    private class NRFTee extends Timber.Tree {
+
+        private ILogSession mLogSession;
+
+        public NRFTee(Context ctx) {
+            mLogSession = Logger.newSession(ctx, "key", "DronePan");
+
+        }
+
+        @Override
+        protected void log(int priority, String tag, String message, Throwable t) {
+            switch (priority) {
+                case Log.DEBUG:
+                    Logger.d(mLogSession, message);
+                    break;
+                case Log.VERBOSE:
+                    Logger.v(mLogSession, message);
+                    break;
+                case Log.INFO:
+                    Logger.i(mLogSession, message);
+                    break;
+                case Log.WARN:
+                    Logger.w(mLogSession, message);
+                    break;
+                case Log.ERROR:
+                    Logger.e(mLogSession, message);
+                    break;
+                default:
+                    Logger.w(mLogSession, message);
+            }
         }
     }
-
-
 }
